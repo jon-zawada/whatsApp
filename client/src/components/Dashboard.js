b1e2b9889e3bbbfbcfa3a7189160e1fea0552cde
jon-zawada <jonathan_zawada@yahoo.com>
2024-05-06 23:47:26 -0700

    sidebar

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -4,7 +4,7 @@ import Sidebar from "./Sidebar";
 export default function Dashboard({ id }) {
   return (
     <div className="d-flex" style={{ height: "100vh" }}>
-      <Sidebar />
+      <Sidebar id={id}/>
     </div>
   );
 }
